8cae52300a28f5e5a01ff7683d46b348144190a3
Ddloyd <quocanh25022001@gmail.com>
2023-02-03 14:31:44 +0700

    first commit

--- java/src/ss15_exception_debug/baitap/triangle/Triangle.java
+++ java/src/ss15_exception_debug/baitap/triangle/Triangle.java
@@ -6,22 +6,30 @@ public class Triangle {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         TriangleEdgesCheck check = new TriangleEdgesCheck();
-        try {
-            System.out.print("Nhập cạnh thứ nhất: ");
-            double a = Double.parseDouble(scanner.nextLine());
-            System.out.print("Nhập cạnh thứ hai: ");
-            double b = Double.parseDouble(scanner.nextLine());
-            System.out.print("Nhập cạnh thứ ba: ");
-            double c = Double.parseDouble(scanner.nextLine());
-            check.checkTriangleEdges(a, b, c);
-        } catch (IllegalTriangleException e) {
-            System.out.println(e.getMessage());
-        } catch (NumberFormatException e) {
-            System.out.println("Không được nhập chữ");
-        } catch (Exception e) {
-            System.out.println("Lỗi: Không đúng định dạng ");
-        } finally {
-            System.out.println("Kết thúc chương trình");
-        }
+        boolean flag;
+        do {
+            try {
+                System.out.print("Nhập cạnh thứ nhất: ");
+                double a = Double.parseDouble(scanner.nextLine());
+                System.out.print("Nhập cạnh thứ hai: ");
+                double b = Double.parseDouble(scanner.nextLine());
+                System.out.print("Nhập cạnh thứ ba: ");
+                double c = Double.parseDouble(scanner.nextLine());
+                flag = false;
+                check.checkTriangleEdges(a, b, c);
+            } catch (IllegalTriangleException e) {
+                System.out.println(e.getMessage());
+                flag = true;
+            } catch (NumberFormatException e) {
+                System.out.println("Không được nhập chữ");
+                flag = true;
+            } catch (Exception e) {
+                System.out.println("Lỗi: Không đúng định dạng ");
+                flag = true;
+            } finally {
+                System.out.println("Kết thúc chương trình");
+            }
+        } while (flag);
     }
 }
+
