5f18d85b8efdba20ad8e4195682136811bfd4f62
Ddloyd <quocanh25022001@gmail.com>
2023-02-03 11:29:09 +0700

    first commit

---
+++ java/src/ss15_exception_debug/baitap/triangle/IllegalTriangleException.java
@@ -0,0 +1,7 @@
+package ss15_exception_debug.baitap.triangle;
+
+public class IllegalTriangleException extends Exception {
+    public IllegalTriangleException(String str) {
+        super(str);
+    }
+}
---
+++ java/src/ss15_exception_debug/baitap/triangle/TriangleEdgesCheck.java
@@ -0,0 +1,14 @@
+package ss15_exception_debug.baitap.triangle;
+
+public class TriangleEdgesCheck {
+    public void checkTriangleEdges(double a, double b, double c) throws IllegalTriangleException {
+        if (a < 0 || b <0 || c < 0){
+            throw new IllegalTriangleException("Lỗi: Cạnh tam giác không thể là số âm");
+        }
+        else if (a + b <= c || a + c <= b || b + c <= a) {
+            throw new IllegalTriangleException("Lỗi: Tam giác không hợp lệ ");
+        } else {
+            System.out.println("Tam giác hợp lệ ");
+        }
+    }
+}
